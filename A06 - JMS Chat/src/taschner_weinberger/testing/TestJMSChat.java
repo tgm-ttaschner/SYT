716b41ed7ba61bee14998dc9bbee37a3815afe29
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-26 18:54:01 +0100

    TestJMSChat:
    Added some test cases

--- A06 - JMS Chat/src/taschner_weinberger/testing/TestJMSChat.java
+++ A06 - JMS Chat/src/taschner_weinberger/testing/TestJMSChat.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import taschner_weinberger.JMSChatCLI;
+
+/**
+ * @author Thomas Taschner
+ * 
+ * In dieser Klasse wird der JMS Chat auf seine korrekte Funktionsweise ueberprueft.
+ *
+ */
 public class TestJMSChat {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOKRun3Args() {
+		String[] main = {"localhost", "test", "room123"};
+		JMSChatCLI.main(main);
+	}
+	
+	@Test
+	public void testOKRun4Args() {
+		String[] main = {"localhost", "test", "room123", "61616"};
+		JMSChatCLI.main(main);
 	}
 
 }
