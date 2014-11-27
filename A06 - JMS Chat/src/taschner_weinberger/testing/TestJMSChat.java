1609ebc667f3c9f1d7466a0b339f2e8a11cb59d3
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2014-11-27 19:57:58 +0100

    TestJMSChat:
    Added some more test cases, reaching 61.4% now

--- A06 - JMS Chat/src/taschner_weinberger/testing/TestJMSChat.java
+++ A06 - JMS Chat/src/taschner_weinberger/testing/TestJMSChat.java
@@ -12,6 +12,8 @@ import org.junit.Test;
 import taschner_weinberger.JMSChatCLI;
 import taschner_weinberger.JMSChatReceiver;
 import taschner_weinberger.JMSChatSender;
+import taschner_weinberger.MailReceiver;
+import taschner_weinberger.MailSender;
 
 /**
  * @author Thomas Taschner
@@ -117,7 +119,7 @@ public class TestJMSChat {
 	public void testChatReceiverRun() {
 		//JMSChatReceiver r = new JMSChatReceiver("localhost", "test123", "abc", 61616);
 
-		byte[] input = "test".getBytes();
+		byte[] input = "test\nEXIT".getBytes();
 		ByteArrayInputStream in = new ByteArrayInputStream(input);
 		System.setIn(in);
 
@@ -126,15 +128,116 @@ public class TestJMSChat {
 
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		//redirect the System-output (normaly the console) to a variable
-		System.setOut(new PrintStream(outContent));
+		//System.setOut(new PrintStream(outContent));
 
 		//call your method here
-		new Thread(new JMSChatSender("localhost", "test123", "abc", 61616)).run();
-		new Thread(new JMSChatReceiver("localhost", "test123", "abc", 61616)).run();
+		new Thread(new JMSChatSender("localhost", "test123", "abc", 61616)).start();
+		new Thread(new JMSChatReceiver("localhost", "test123", "abc", 61616)).start();
 
 		//check if your error message is in the output variable
-		assertEquals("test", outContent.toString());
+		//assertEquals("test", outContent.toString());
 	}
 	
 	*/
+	
+	@Test
+	public void testChatSenderConstructor() {
+		new JMSChatSender("localhost", "test123", "abc", 61616);
+	}
+
+	@Test
+	public void testChatSenderUserAttribute() {
+		JMSChatSender r = new JMSChatSender("localhost", "test123", "abc", 61616);
+		r.setUser("tt");
+		assertEquals("tt", r.getUser());
+	}
+
+	@Test
+	public void testChatSenderPasswordAttribute() {
+		JMSChatSender r = new JMSChatSender("localhost", "test123", "abc", 61616);
+		r.setPassword("letmein");
+		assertEquals("letmein", r.getPassword());
+	}
+
+	@Test
+	public void testChatSenderSubjectAttribute() {
+		JMSChatSender r = new JMSChatSender("localhost", "test123", "abc", 61616);
+		r.setSubject("topic123");
+		assertEquals("topic123", r.getSubject());
+	}
+
+	@Test
+	public void testChatSenderIPAttribute() {
+		JMSChatSender r = new JMSChatSender("localhost", "test123", "abc", 61616);
+		r.setIp("1.2.3.4");
+		assertEquals("1.2.3.4", r.getIp());
+	}
+
+	@Test
+	public void testChatSenderPortAttribute() {
+		JMSChatSender r = new JMSChatSender("localhost", "test123", "abc", 61616);
+		r.setPort(1234);
+		assertEquals(1234, r.getPort());
+	}
+
+	@Test
+	public void testChatSenderURLAttribute() {
+		JMSChatSender r = new JMSChatSender("localhost", "test123", "abc", 61616);
+		r.setUrl("failover://tcp://" + "4.5.6.7" + ":" + 6789);
+		assertEquals("failover://tcp://" + "4.5.6.7" + ":" + 6789, r.getUrl());
+	}
+	
+	
+	@Test
+	public void testReceiverConstructor() {
+		new MailReceiver("localhost", 61616);
+	}
+
+	@Test
+	public void testReceiverIPAttribute() {
+		MailReceiver r = new MailReceiver("localhost", 61616);
+		r.setIp("1.2.3.4");
+		assertEquals("1.2.3.4", r.getIp());
+	}
+
+	@Test
+	public void testReceiverPortAttribute() {
+		MailReceiver r = new MailReceiver("localhost", 61616);
+		r.setPort(1234);
+		assertEquals(1234, r.getPort());
+	}
+	
+	@Test
+	public void testSenderConstructor() {
+		new MailSender("localhost", 61616, "hallo");
+	}
+
+	@Test
+	public void testSenderIPAttribute() {
+		MailSender r = new MailSender("localhost", 61616, "hallo");
+		r.setIp("1.2.3.4");
+		assertEquals("1.2.3.4", r.getIp());
+	}
+	
+	@Test
+	public void testUsernameAttribute() {
+		MailSender r = new MailSender("localhost", 61616, "hallo");
+		r.setUsername("tt");
+		assertEquals("tt", r.getUsername());
+	}
+	
+	@Test
+	public void testTextmessageAttribute() {
+		MailSender r = new MailSender("localhost", 61616, "hallo");
+		r.setTextmessage("tag");
+		assertEquals("tag", r.getTextmessage());
+	}
+
+	@Test
+	public void testReceiverSenderPortAttribute() {
+		MailSender r = new MailSender("localhost", 61616, "hallo");
+		r.setPort(1234);
+		assertEquals(1234, r.getPort());
+	}
+	
 }
