a5c846310fd4e742d1d258ebeee800c7db02461c
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-04 18:23:03 +0100

    Added documentation for Client

--- A07 - PI Calculator/src/at/tm/rmi/client/Client.java
+++ A07 - PI Calculator/src/at/tm/rmi/client/Client.java
@@ -4,6 +4,14 @@ import java.rmi.registry.*;
 
 import at.tm.rmi.server.Calculator;
 
+/**
+ * @author Patrik Malik
+ * @author Thomas Taschner
+ * @version 04.01.2015
+ * 
+ * The client connects to the server to get the specified amount of digits of pi using an ip address, a port number and the amount of digits that should be calculated.
+ *
+ */
 public class Client {
 	
 	private String ip;
@@ -11,12 +19,21 @@ public class Client {
 	
 	private int decimal_places;
 	
+	/**
+	 * @param ip the server's ip address
+	 * @param port the server's port number
+	 * @param decimal_places the amount of pi's decimal places you want the server to calculate
+	 */
 	public Client(String ip, int port, int decimal_places)	{
 		this.ip = ip;
 		this.port = port;
 		this.decimal_places = decimal_places;
 	}
-
+	
+	/**
+	 * Locates the registry, looks up a remote reference's name and prints out the response.
+	 * If no connection can be established then an exception is thrown.
+	 */
 	public void connect()	{
 		try {
 			Registry registry = LocateRegistry.getRegistry(this.ip, this.port);
