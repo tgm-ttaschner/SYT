eecc510afcac47adbb0236d06f043e0b0d9c1f7e
Patrick Malik <austria.p@hotmail.com>
2015-01-04 19:46:00 +0100

    Changed strategy.

---
+++ A07 - PI Calculator/src/at/tm/rmi/server/CalculatorBalancer.java
@@ -0,0 +1,83 @@
+package at.tm.rmi.server;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+public class CalculatorBalancer implements Calculator {
+
+	private List<Calculator> implementations;
+	private int count;
+	private int port;
+
+	public CalculatorBalancer(int port) {
+		this.port = port;
+		count = 1;
+
+		// example //www.facebook.com:5052
+		Registry registry = null;
+		try {
+			registry = LocateRegistry.createRegistry(this.getPort());
+		} catch (RemoteException e) {
+			System.out.println("A problem occured while creating a registry");
+			System.exit(314);
+		}
+		try {
+			registry.rebind("Balancer", this);
+			registry.rebind("Calculator", this);
+		} catch (RemoteException e) {
+			System.out.println("A problem occured while binding to its registry.");
+			System.exit(3141);
+		}
+
+	}
+
+	@Override
+	public BigDecimal pi(int anzahl_nachkommastellen) throws RemoteException {
+
+		Calculator currImplementation = implementations.get(count % implementations.size());
+		count++;
+
+		// Calculator stub = ;
+		// try {
+		// stub = (Calculator) registry.lookup("Calculator");
+		// } catch (NotBoundException e) {
+		//
+		// }
+
+		return currImplementation.pi(anzahl_nachkommastellen);
+	}
+
+	public void addImplementation(Calculator implementation) {
+		this.getImplementations().add(implementation);
+	}
+
+	public List<Calculator> getImplementations() {
+		return implementations;
+	}
+
+	public void setImplementations(List<Calculator> implementations) {
+		this.implementations = implementations;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
--- A07 - PI Calculator/src/at/tm/rmi/server/Server.java
+++ A07 - PI Calculator/src/at/tm/rmi/server/Server.java
@@ -1,27 +1,51 @@
 package at.tm.rmi.server;
 
+import java.net.URI;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
 
 	private Calculator calc;
 	private int port;
 
-	public Server(int port) {
+	public Server(int port, URI balancer, Calculator calcimpl) throws RemoteException, NotBoundException {
 		this.port = port;
+		
+		Registry registry = LocateRegistry.getRegistry(balancer.getHost(), balancer.getPort());
+		CalculatorBalancer bal = (CalculatorBalancer) registry.lookup("Balancer");
+		bal.addImplementation(calcimpl);
 	}
 
-	public void bindToRegistry(Calculator calcimpl) throws RemoteException {
+	// public void bindToRegistry(Calculator calcimpl) throws RemoteException {
+	//
+	// Calculator stub = (Calculator) UnicastRemoteObject.exportObject(calcimpl,
+	// this.port);
+	//
+	// Registry registry = LocateRegistry.createRegistry(this.port);
+	// registry.rebind("Calculator", stub);
+	// calc = stub;
+	// System.err.println("Server ready");
+	// }
 
-		Calculator stub = (Calculator) UnicastRemoteObject.exportObject(calcimpl, this.port);
-		
-		Registry registry = LocateRegistry.createRegistry(this.port);
-		registry.rebind("Calculator", stub);
-		calc = stub;
-		
-		System.err.println("Server ready");
+	public Calculator getCalc() {
+		return calc;
+	}
+
+	public void setCalc(Calculator calc) {
+		this.calc = calc;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
 	}
 }
