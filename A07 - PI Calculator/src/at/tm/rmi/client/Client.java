dc638c264993d00d70ba253010e3ccfe2fb0e476
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-04 17:18:36 +0100

    Simplified imports Moved client connection code into connect method

--- A07 - PI Calculator/src/at/tm/rmi/client/Client.java
+++ A07 - PI Calculator/src/at/tm/rmi/client/Client.java
@@ -1,21 +1,13 @@
 package at.tm.rmi.client;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.rmi.registry.*;
 
 import at.tm.rmi.server.Calculator;
 
 public class Client {
 
-	private Client() {
-
-	}
-
-	public static void main(String[] args) {
-
-		// String host = (args.length < 1) ? null : args[0];
+	public void connect()	{
 		try {
-
 			Registry registry = LocateRegistry.getRegistry("192.168.0.22", 5052);
 			Calculator stub = (Calculator) registry.lookup("Calculator");
 			System.out.println("response: " + stub.pi(-1));
