4a118cc3da75f3eab9d80bc84819618ee2916c27
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-04 17:43:43 +0100

    Added meaningful constructor Added attributes (ip, port, decimal_places) Connect method now uses those attributes

--- A07 - PI Calculator/src/at/tm/rmi/client/Client.java
+++ A07 - PI Calculator/src/at/tm/rmi/client/Client.java
@@ -5,12 +5,23 @@ import java.rmi.registry.*;
 import at.tm.rmi.server.Calculator;
 
 public class Client {
+	
+	private String ip;
+	private int port;
+	
+	private int decimal_places;
+	
+	public Client(String ip, int port, int decimal_places)	{
+		this.ip = ip;
+		this.port = port;
+		this.decimal_places = decimal_places;
+	}
 
 	public void connect()	{
 		try {
-			Registry registry = LocateRegistry.getRegistry("192.168.0.22", 5052);
+			Registry registry = LocateRegistry.getRegistry(this.ip, this.port);
 			Calculator stub = (Calculator) registry.lookup("Calculator");
-			System.out.println("response: " + stub.pi(-1));
+			System.out.println("response: " + stub.pi(this.decimal_places));
 		} catch (Exception e) {
 			System.err.println("Client exception: " + e.toString());
 			e.printStackTrace();
