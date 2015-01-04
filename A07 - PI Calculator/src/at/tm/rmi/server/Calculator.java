aa5f6f8283c9ad7443e9f6db0ba5a556e8206384
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-04 18:10:12 +0100

    Added documentation for the Calculator Interface

--- A07 - PI Calculator/src/at/tm/rmi/server/Calculator.java
+++ A07 - PI Calculator/src/at/tm/rmi/server/Calculator.java
@@ -1,9 +1,16 @@
 package at.tm.rmi.server;
 
 import java.math.BigDecimal;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+import java.rmi.*;
 
-public interface Calculator extends Remote{
+/**
+ * @author Patrik Malik
+ * @author Thomas Taschner
+ * 
+ * Interface which inherits from Remote. It's only method calciulates PI.
+ * To achieve that it takes the amount of decimal places (of PI) as an input parameter and returns PI up to a certain decimal place as a BigInteger. 
+ *
+ */
+public interface Calculator extends Remote	{
 	public BigDecimal pi (int anzahl_nachkommastellen) throws RemoteException;
 }
