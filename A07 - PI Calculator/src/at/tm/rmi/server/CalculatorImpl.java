fe1f91ac5c737d309d848ac64c605eb095596981
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-05 15:10:15 +0100

    Simplified imports Added documentation for CalculatorImpl

--- A07 - PI Calculator/src/at/tm/rmi/server/CalculatorImpl.java
+++ A07 - PI Calculator/src/at/tm/rmi/server/CalculatorImpl.java
@@ -1,35 +1,48 @@
 package at.tm.rmi.server;
+
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.math.*;
 
 /**
  * @author Thomas Taschner
  * 
- * Geklaut von http://stackoverflow.com/questions/8370290/generating-pi-to-nth-digit-java
+ * Taken from http://stackoverflow.com/questions/8370290/generating-pi-to-nth-digit-java, made some slight changes so it fits to our interface.
+ * 
+ * This class calculates the decimal places of PI with arbitrary precision.
+ * The used algorithm is hard to understand and explain so I won't even bother doing that.
  */
-public class CalculatorImpl implements Calculator,Serializable{
+@SuppressWarnings("serial")
+public class CalculatorImpl implements Calculator, Serializable	{
 
 	private static final BigDecimal TWO = new BigDecimal("2");
 	private static final BigDecimal FOUR = new BigDecimal("4");
 	private static final BigDecimal FIVE = new BigDecimal("5");
 	private static final BigDecimal TWO_THIRTY_NINE = new BigDecimal("239");
 
-
+	/* (non-Javadoc)
+	 * @see at.tm.rmi.server.Calculator#pi(int)
+	 * 
+	 * Takes the amount of decimal places as an input and returns the desired amount of decimal places of PI.
+	 */
 	public BigDecimal pi(int numDigits) {
 		int calcDigits = numDigits + 10;
 		return FOUR.multiply((FOUR.multiply(arccot(FIVE, calcDigits))).subtract(arccot(TWO_THIRTY_NINE, calcDigits))).setScale(numDigits, RoundingMode.DOWN);
 	}
 
+	/**
+	 * @param x a BigInteger number
+	 * @param numDigits the amount of decimal places of PI that will be calculated
+	 * @return sum of some complex arithmetic operation
+	 */
 	private static BigDecimal arccot(BigDecimal x, int numDigits) {
 
 		BigDecimal unity = BigDecimal.ONE.setScale(numDigits, RoundingMode.DOWN);
 		BigDecimal sum = unity.divide(x, RoundingMode.DOWN);
 		BigDecimal xpower = new BigDecimal(sum.toString());
 		BigDecimal term = null;
-		
+
 		boolean add = false;
-		
+
 		for (BigDecimal n = new BigDecimal("3"); term == null || term.compareTo(BigDecimal.ZERO) != 0; n = n.add(TWO)) {
 			xpower = xpower.divide(x.pow(2), RoundingMode.DOWN);
 			term = xpower.divide(n, RoundingMode.DOWN);
@@ -38,5 +51,4 @@ public class CalculatorImpl implements Calculator,Serializable{
 		}
 		return sum;
 	}
-
 }
