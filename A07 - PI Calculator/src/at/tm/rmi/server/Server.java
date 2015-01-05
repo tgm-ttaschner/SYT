72c6055960eb6083d14ea02e90b56ed42dcde5b4
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-05 15:58:51 +0100

    Removed unneccessary code Removed unneccessary imports Simplified imports Code cleanup Added documentation Switched order of input parameters of connect (more intuitive)

--- A07 - PI Calculator/src/at/tm/rmi/server/Server.java
+++ A07 - PI Calculator/src/at/tm/rmi/server/Server.java
@@ -1,14 +1,22 @@
 package at.tm.rmi.server;
 
 import java.net.URI;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.HashMap;
-import java.util.Map;
+import java.rmi.registry.*;
 
+/**
+ * @author Patrick Malik
+ * @author Thomas Taschner
+ * @version 05.01.2015
+ * 
+ * Starts a server on a given port with a given name.
+ * It connects with a balancer via the balancers URL and transmits him a calculator object.
+ * To achieve that the server gets the balancers registry and looks a balancer object up.
+ * Then the server adds itself to the list (map) of available balancers and rebinds the balancer as a balancer and a calculator to the registry.
+ * The disconnect procedure works the same way.
+ * The only thing that is different is that the server doesn't need to get the balancers registry, also it gets removed instead of added.
+ * An error is printed if anything should go wrong.
+ * This class also provides getter and setter methods for its attributes.
+ */
 public class Server {
 
 	private Calculator calc;
@@ -18,14 +26,25 @@ public class Server {
 	
 	private Registry registry;
 
+	/**
+	 * @param port the port the server will start on
+	 * @param name the name the server will have
+	 * 
+	 * Creates a server on a given port with a given name.
+	 */
 	public Server(int port, String name) {
 		this.port = port;
 		this.name = name;
-		
-		
 	}
 	
-	public void connect(Calculator calcimpl, URI balancer)	{
+	/**
+	 * @param balancer the balancers URI (\\ IP or URL:port)
+	 * @param calcimpl the calculator object that will be added to the balancer
+	 * 
+	 * Gets the given balancers registry, adds the calculator object to the balancers list and rebinds the balancer.
+	 * Prints out the error if one should occur.
+	 */
+	public void connect(URI balancer, Calculator calcimpl)	{
 		try {
 			registry = LocateRegistry.getRegistry(balancer.getHost(), balancer.getPort());
 			CalculatorBalancer bal = (CalculatorBalancer) registry.lookup("Balancer");
@@ -38,6 +57,11 @@ public class Server {
 		
 	}
 	
+	/**
+	 * Disconnects a calculator from the balancer.
+	 * The balancers registry is looked up, the current server is removed and the balancer is rebound.
+	 * Prints out the error if one should occur.
+	 */
 	public void disconnect()	{
 		try {
 			CalculatorBalancer bal = (CalculatorBalancer) registry.lookup("Balancer");
@@ -49,21 +73,23 @@ public class Server {
 		}
 	}
 
-	// public void bindToRegistry(Calculator calcimpl) throws RemoteException {
-	//
-	// Calculator stub = (Calculator) UnicastRemoteObject.exportObject(calcimpl,
-	// this.port);
-	//
-	// Registry registry = LocateRegistry.createRegistry(this.port);
-	// registry.rebind("Calculator", stub);
-	// calc = stub;
-	// System.err.println("Server ready");
-	// }
-
+	/**
+	 * @return the calculator object
+	 * 
+	 * Getter for calculator.
+	 */
 	public Calculator getCalc() {
 		return calc;
 	}
 
+	/**
+	 * @param calc the calculator object
+	 * 
+	 * Setter for calculator.
+	 * 
+	 * The balancers registry is looked up, the calculator object added to the balancers list and the balancer gets rebound to the registry.
+	 * Prints out the error if one should occur and terminates the program.
+	 */
 	public void setCalc(Calculator calc) {
 		this.calc = calc;
 		
@@ -73,33 +99,62 @@ public class Server {
 			bal.addImplementation(this.name, this.calc);
 			registry.rebind("Balancer", bal);
 			registry.rebind("Calculator", bal);
-		} catch (RemoteException | NotBoundException e) {
+		} catch (Exception e) {
 			System.out.println("An Error occured");
 			System.exit(314159);
 		}
-		
 	}
 
+	/**
+	 * @return the port the server will run on
+	 * 
+	 * Getter for port.
+	 */
 	public int getPort() {
 		return port;
 	}
 
+	/**
+	 * @param port the port the server will run on
+	 * 
+	 * Setter for port.
+	 */
 	public void setPort(int port) {
 		this.port = port;
 	}
 
+	/**
+	 * @return the servers name
+	 * 
+	 * Getter for name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the servers name
+	 * 
+	 * Setter for name.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the balancers registry
+	 * 
+	 * Getter for registry.
+	 */
 	public Registry getRegistry() {
 		return registry;
 	}
 
+	/**
+	 * @param registry the balancers registry
+	 * 
+	 * Setter for registry.
+	 */
 	public void setRegistry(Registry registry) {
 		this.registry = registry;
 	}
