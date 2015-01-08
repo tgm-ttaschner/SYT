a65a027dd504e3852db30bc602a71313ebf1ad14
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-08 23:40:55 +0100

    Added client test cases

--- A07 - PI Calculator/src/at/tm/rmi/test/client/TestClient.java
+++ A07 - PI Calculator/src/at/tm/rmi/test/client/TestClient.java
@@ -2,22 +2,71 @@ package at.tm.rmi.test.client;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.rmi.RemoteException;
+import java.util.logging.Logger;
+
 import org.junit.*;
 
 import at.tm.rmi.client.Client;
+import at.tm.rmi.server.CalculatorBalancer;
+import at.tm.rmi.server.CalculatorImpl;
+import at.tm.rmi.server.Server;
 
 public class TestClient {
 	
+	static CalculatorBalancer cb;
 	Client c;
+	static Server s;
 	
-	@Before
-	public void setup()	{
-		
+	@BeforeClass
+	public static void setup() throws RemoteException, URISyntaxException	{
+		cb = new CalculatorBalancer(5052);
+		s = new Server("Server 1", 5053, new CalculatorImpl());
+		s.connect(new URI("//localhost:5052"));
 	}
 
 	@Test
-	public void testValidConnection2Decimals() {
-		c = new Client(address, decimal_places);
+	public void testValidConnection2Decimals() throws IllegalArgumentException, URISyntaxException, RemoteException {
+		c = new Client(new URI("//localhost:5052"), 2);
+		
+		assertEquals("response: 3.14", c.connect());
 	}
-
+	
+	@Test
+	public void testValidConnection0Decimals() throws IllegalArgumentException, URISyntaxException, RemoteException {
+		c = new Client(new URI("//localhost:5052"), 0);
+		
+		assertEquals("response: 3", c.connect());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testValidConnectionNegative1Decimals() throws IllegalArgumentException, URISyntaxException, RemoteException {
+		c = new Client(new URI("//localhost:5052"), -1);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testValidConnection0Port() throws IllegalArgumentException, URISyntaxException, RemoteException {
+		c = new Client(new URI("//localhost:0"), 5);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testValidConnection1Port() throws IllegalArgumentException, URISyntaxException, RemoteException {
+		c = new Client(new URI("//localhost:0"), -50);
+	}
+	
+	@Test
+	public void testInvalidConnection() throws IllegalArgumentException, URISyntaxException, RemoteException {
+		c = new Client(new URI("//remotehost:5052"), 5055);
+		
+		//ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		
+		//System.setOut(new PrintStream(out));
+		
+		//System.setOut(null);
+	}
 }
--- A07 - PI Calculator/src/at/tm/rmi/test/client/TestRun.java
+++ A07 - PI Calculator/src/at/tm/rmi/test/client/TestRun.java
@@ -1,14 +1,43 @@
 package at.tm.rmi.test.client;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import at.tm.rmi.client.Run;
 
 public class TestRun {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@BeforeClass
+	public static void setup()	{
+
 	}
 
+	@Test
+	public void runOkBalancer() {
+		String[] runbalancer = {"-b", "-p", "5052"};
+
+		Run.main(runbalancer);
+		
+		//System.setOut(new PrintStream(new ByteArrayOutputStream()));
+		
+	}
+
+	@Test
+	public void runOkServer() {
+		
+		String[] runserver = {"-s", "-h", "localhost", "-p", "5052", "-n", "Server", "-S", "2", "-P", "5055"};
+
+		Run.main(runserver);
+		
+	}
+
+	@Test
+	public void runOkClient() {
+		String[] runserver = {"-c", "-h", "localhost", "-p", "5052", "-C", "1", "-d", "1"};
+
+		Run.main(runserver);
+	}
 }
