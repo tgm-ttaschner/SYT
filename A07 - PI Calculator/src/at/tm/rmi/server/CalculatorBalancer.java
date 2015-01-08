1daf78b6a7dd28e82c47513fb879ad93eb6d6d31
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-08 01:35:00 +0100

    Updated and added addition documentation Simplified imports

--- A07 - PI Calculator/src/at/tm/rmi/server/CalculatorBalancer.java
+++ A07 - PI Calculator/src/at/tm/rmi/server/CalculatorBalancer.java
@@ -1,9 +1,7 @@
 package at.tm.rmi.server;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
+import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,22 +11,19 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author Thomas Taschner
  * @version 05.01.2015
  * 
- *          A Balancer which also can calculate the decimal places of PI. Upon
- *          initialization it binds itself to the registry in two ways, as a
- *          balancer and a calculator. An exception is thrown if an error should
- *          occur.
+ * A Balancer which also can calculate the decimal places of PI.
+ * Upon initialization it binds itself in form of an exported UnicastRemoteObject to the registry in two ways, as a balancer and a calculator.
+ * An exception is thrown if an error should occur.
  * 
- *          The round robin principle is used to balance the workload by using a
- *          counter. The server for calculating the workload gets chosen by
- *          taking the remainder of dividing count through the amount of
- *          available servers. This way the workload gets distributed evenly.
+ * The round robin principle is used to balance the workload by using a counter.
+ * The server for calculating the workload gets chosen by taking the remainder of dividing count through the amount of available servers.
+ * This way the workload gets distributed evenly.
  * 
- *          A ConcurrentHashMap (thread safe) is used to store the object
- *          together with its name. An additional String array only consisting
- *          of the keys is used for navigation (a navigable concurrent HashMap
- *          would be a big help). This class also provides getter and setter
- *          methods for all its attributes.
+ * A ConcurrentHashMap (thread safe) is used to store the object together with its name.
+ * An additional String array only consisting of the keys is used for navigation (a navigable concurrent HashMap would be a big help).
+ * This class also provides getter and setter methods for all its attributes.
  * 
+ * An appropriate error message will be printed out if something goes wrong.
  */
 @SuppressWarnings("serial")
 public class CalculatorBalancer implements BalancerInterface {
@@ -38,13 +33,12 @@ public class CalculatorBalancer implements BalancerInterface {
 	private int port;
 
 	/**
-	 * @param port
-	 *            the port on which the balancer starts
+	 * Sets up a balancer on a specified port.
+	 * The constructor creates a registry on the given port and binds itself in form of an exported UnicastRemoteObject as a balancer and calculator to it.
+	 * A RemoteException is caught if the registry cannot be accessed for any reason.
 	 * 
-	 *            Sets up a balancer on a specified port. The constructor
-	 *            creates a registry on the given port and binds itself as a
-	 *            balancer and calculator to it. An exception is thrown and the
-	 *            program terminates itself if an error occurrs.
+	 * @param port the port the balancer operates on
+	 * @throws RemoteException is thrown when a remote error occurrs (e.g. no connection to the server)
 	 */
 	public CalculatorBalancer(int port) throws RemoteException {
 
@@ -67,16 +61,8 @@ public class CalculatorBalancer implements BalancerInterface {
 			registry.rebind("Calculator", UnicastRemoteObject.exportObject(this, this.getPort()));
 		} catch (RemoteException e) {
 			System.out.println("A problem occured while binding to its registry.");
-			e.printStackTrace();
 			System.exit(3141);
 		}
-
-		// try {
-		// Thread.sleep(120000);
-		// } catch (InterruptedException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
 
 	}
 
@@ -85,15 +71,14 @@ public class CalculatorBalancer implements BalancerInterface {
 	 * 
 	 * @see at.tm.rmi.server.Calculator#pi(int)
 	 * 
-	 * Input: the amount of decimal places of PI Output: the calculated decimal
-	 * places of PI
+	 * Input: the amount of decimal places of PI Output: the calculated decimal places of PI
 	 * 
-	 * The calculator objects and their names are stored in a ConcurrentHashMap
-	 * (thread safe HashMap). For better navigatability (gibts das Wort
-	 * überhaupt?) the objects names are additionally stored in an String array.
-	 * Then the calculator object gets assigned to a PI Server and count gets
-	 * incremented. Finally the desired decimal places for the calculator object
-	 * get returned.
+	 * The calculator objects and their names are stored in a ConcurrentHashMap (thread safe HashMap). 
+	 * For better navigation the objects names are additionally stored in an String array.
+	 * Then the calculator object gets assigned to a PI Server and count gets incremented.
+	 * Finally the desired decimal places for the calculator object get returned.
+	 * 
+	 * A NotBoundException is caught if the registry cannot be loaded and/or looked up.
 	 */
 	@Override
 	public BigDecimal pi(int anzahl_nachkommastellen) throws RemoteException {
@@ -104,57 +89,40 @@ public class CalculatorBalancer implements BalancerInterface {
 
 		ServerInterface currServer = servers.get(keys[count % servers.size()]);
 
-		// System.out.println(keys[count % implementations.size()]);
-
 		count++;
 
-		// System.out.println(count);
 		Calculator cal = null;
 		try {
 			cal = (Calculator) currServer.getRegistry().lookup("Calculator");
 		} catch (NotBoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("An error occurred while trying to load or lookup the registry");
 		}
 		return cal.pi(anzahl_nachkommastellen);
 	}
 
-	/**
-	 * @param key
-	 *            the objects name
-	 * @param implementation
-	 *            the calculator object
-	 * 
-	 *            Adds a calculator object together with its name.
+	/* (non-Javadoc)
+	 * @see at.tm.rmi.server.BalancerInterface#addServer(java.lang.String, at.tm.rmi.server.ServerInterface)
 	 */
 	public void addServer(String key, ServerInterface server) {
 		this.getServers().put(key, server);
 	}
 
-	/**
-	 * @param key
-	 *            the objects name
-	 * 
-	 *            Removes an object from the map via its key (name).
+	/* (non-Javadoc)
+	 * @see at.tm.rmi.server.BalancerInterface#removeServer(java.lang.String)
 	 */
 	public void removeServer(String key) {
 		this.getServers().remove(key);
 	}
 
-	/**
-	 * @return the map itself
-	 * 
-	 *         Getter for the ConcurrentHashMap.
+	/* (non-Javadoc)
+	 * @see at.tm.rmi.server.BalancerInterface#getServers()
 	 */
 	public ConcurrentHashMap<String, ServerInterface> getServers() {
 		return servers;
 	}
 
-	/**
-	 * @param implementations
-	 *            the map
-	 * 
-	 *            Setter for the ConcurrentHashMap.
+	/* (non-Javadoc)
+	 * @see at.tm.rmi.server.BalancerInterface#setServers(java.util.concurrent.ConcurrentHashMap)
 	 */
 	public void setServers(ConcurrentHashMap<String, ServerInterface> servers) {
 		this.servers = servers;
@@ -163,17 +131,16 @@ public class CalculatorBalancer implements BalancerInterface {
 	/**
 	 * @return count (the counter)
 	 * 
-	 *         Getter for count.
+	 * Getter for count.
 	 */
 	public int getCount() {
 		return count;
 	}
 
 	/**
-	 * @param count
-	 *            the counter variable
+	 * @param count the counter variable
 	 * 
-	 *            Setter for count.
+	 * Setter for count.
 	 */
 	public void setCount(int count) {
 		CalculatorBalancer.count = count;
@@ -182,17 +149,16 @@ public class CalculatorBalancer implements BalancerInterface {
 	/**
 	 * @return the port
 	 * 
-	 *         Getter for port.
+	 * Getter for port.
 	 */
 	public int getPort() {
 		return port;
 	}
 
 	/**
-	 * @param port
-	 *            the port
+	 * @param port the port
 	 * 
-	 *            Setter for Port.
+	 * Setter for Port.
 	 */
 	public void setPort(int port) {
 		this.port = port;
