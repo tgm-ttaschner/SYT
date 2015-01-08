972c09101f7525c2e9a32aac23dc9f77fe082b43
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-08 20:50:24 +0100

    Added documentation for MySecurityManager

--- A07 - PI Calculator/src/at/tm/rmi/utils/MySecurityManager.java
+++ A07 - PI Calculator/src/at/tm/rmi/utils/MySecurityManager.java
@@ -2,6 +2,13 @@ package at.tm.rmi.utils;
 
 import java.security.Permission;
 
+/**
+ * @author Patrieg Malick
+ * @version 08.01.2015
+ * 
+ * The only purpose of this class is to fool the original SecurityManager by returning nothing and let us do our job.
+ *
+ */
 public class MySecurityManager extends SecurityManager {
     @Override
     public void checkPermission(Permission perm) {
